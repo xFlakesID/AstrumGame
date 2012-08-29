baacc3ab57380f8fbe7abe014f86432cce20f1ca
Micha Janssen <f2pview@gmail.com>
2012-08-29 23:24:36 +0200

    SMOOTHEND TEXT IN SCREENMANAGER
    Made variable g from Graphic element to Graphics2D element to antialias
    it!

--- src/Astrum/Main.java
+++ src/Astrum/Main.java
@@ -33,7 +33,12 @@ public class Main extends JFrame {
 			}
 		}
 		
+		//paint with the paint method
 		public void paint(Graphics g){
+			if(g instanceof Graphics2D){
+				Graphics2D g2 = (Graphics2D) g;
+				g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+			}
 			g.drawString("Hello Rob",  200, 200);
 		}
 		
